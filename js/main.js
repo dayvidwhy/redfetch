00b74ef6485d03ad6c1ea03bb71b44aa8fe677bb
David Young <dayvidwhy@gmail.com>
2016-12-27 20:49:11 +1000

    Add overlay on image clicks.

--- js/main.js
+++ js/main.js
@@ -15,21 +15,27 @@ var currentURL = baseURL;
 var debounceTimer = null;
 var debounceDelay = 100;
 
+init();
 // start
-fetchReddit(currentURL);
+function init() {
+    // setup overlay dismiss
+    document.getElementById('overlay').onclick = function() {
+        this.style.display = 'none';
+        document.body.style.overflow = ''; // let body scroll again
+    };
+    fetchReddit(currentURL);
+}
 
 /*
 * Initiate our Reddit request
 */
 function fetchReddit(currentURL) {
-    console.log(currentURL);
     fetch(currentURL).then(function(response) {
         var contentType = response.headers.get("content-type");
         if(contentType && contentType.indexOf("application/json") !== -1) {
             return response.json().then(redditLoaded);
         } else {
             // probably a bad subreddit
-            console.log("json failed to load");
         }
     });
 }
@@ -38,7 +44,6 @@ function fetchReddit(currentURL) {
 * When our JSON successfully loads parse it and render images on the page.
 */
 function redditLoaded(json) {
-    console.log(json);
     count += 25;
     currentURL = baseURL + '?count=' + count + '&after=' + json.data.after;
     var output = document.getElementById("output");
@@ -46,7 +51,7 @@ function redditLoaded(json) {
     var str = '';
     var row, container, image;
     for (var i = 1; i < len; i++) {
-        if ((i - 1) % 6 === 0) {
+        if ((i - 1) % 4 === 0) {
             // starting a row
             if (row) output.appendChild(row);
             row = document.createElement('div');
@@ -55,28 +60,33 @@ function redditLoaded(json) {
 
         // Create the image container
         if (!json.data.children[i].data.preview) continue;
-        var width = json.data.children[i].data.preview.images[0].source.width;
-        var height = json.data.children[i].data.preview.images[0].source.height;
+        var currentImages = json.data.children[i].data.preview.images[0];
+        var currentResolutions = currentImages.resolutions;
+        var largeResolution = currentResolutions[Math.floor(currentResolutions.length - 1 / 2)];
+
+        var width = largeResolution.width;
+        var height = largeResolution.height;
         var aspect = width / height;
         
-        
+        // make the container for the image
         container = document.createElement('div');
         container.className = 'image-container';
         container.style.flex = aspect;
 
         // Create the image with thumbnail
         image = new Image();
-        var thumbnail = json.data.children[i].data.preview.images[0].resolutions[0].url
-        thumbnail = thumbnail.split('&amp;').join('&');
+        var thumbnail = replaceHTMLEscape(currentImages.resolutions[0].url);
         image.src = thumbnail;
         image.className = 'img-loading';
+        // Set the large image for our overlay
+        image.setAttribute('large-image', replaceHTMLEscape(currentImages.source.url));
         
         // when it loads change the src to the bigger one
-        image.onload = (function(index, image) {
+        image.onload = (function(image, largeResolution) {
             return function() {
-                imageLoad(image, json.data.children[index].data.preview.images[0].source.url);
-            }
-        })(i, image);
+                imageLoad(image, largeResolution.url);
+            };
+        })(image, largeResolution);
 
         // if it fails to load delete the element
         image.onerror = imageFail;
@@ -103,11 +113,9 @@ function redditLoaded(json) {
 function scrollLoad(event) {
     clearTimeout(debounceTimer);
     debounceTimer = setTimeout(function() {
-        console.log('scroll');
         if (document.body.scrollHeight == document.body.scrollTop + window.innerHeight) {
             document.removeEventListener('scroll', scrollLoad);
             fetchReddit(currentURL);
-            console.log('fired bottom');
             document.getElementById('img-loading-message').style.display = 'block';
         }
     }, debounceDelay);
@@ -120,16 +128,25 @@ function imageFail() {
     this.outerHTML = '';
 }
 
+function replaceHTMLEscape(string) {
+    return string.split('&amp;').join('&');
+}
+
 /*
 * When the thumbnail loads change it's source to the larger image.
 * This will cause a network request to start and the user can view the
 * blurry thumbnail until it's done.
 */
 function imageLoad(img, large) {
-    console.log(large);
-    large = large.split('&amp;').join('&');
+    large = replaceHTMLEscape(large);
     img.src = large;
     img.onload = function() {
         this.className = 'img-zoom';
-    }
+    };
+    img.onclick = function() {
+        // display the overlay
+        document.getElementById('overlay').style.display = 'block';
+        document.getElementById('overlay-img').src = this.getAttribute('large-image');
+        document.body.style.overflow = 'hidden'; // don't let body scroll
+    };
 }
